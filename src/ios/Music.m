b09bc75fd84a246867be8586a95088d8ffbc0bae
Ahmed Raza <hafizahmedattari@gmail.com>
2015-05-18 21:08:35 +0500

    Added playSong method with onComplete callback

--- src/ios/Music.m
+++ src/ios/Music.m
@@ -36,8 +36,8 @@
     NSMutableArray* songsArray = [NSMutableArray arrayWithCapacity:1];
     
     for (MPMediaItem *song in songs) {
-        NSString *songtId = [song valueForProperty: MPMediaItemPropertyPersistentID];
-        NSString *songName = [song valueForProperty: MPMediaItemPropertyTitle];
+        NSString *songtId = [NSString stringWithFormat:@"%@", [song valueForProperty: MPMediaItemPropertyPersistentID]];
+        NSString *songName = [NSString stringWithFormat:@"%@", [song valueForProperty: MPMediaItemPropertyTitle]];
         
         NSMutableDictionary* so = [NSMutableDictionary dictionaryWithCapacity:1];
         
@@ -46,6 +46,7 @@
         
         [songsArray addObject:so];
     }
+    
     NSLog(@"%@", songsArray);
     
     CDVPluginResult* pluginResult = [CDVPluginResult
@@ -73,37 +74,86 @@
     NSMutableArray* songsArray = [NSMutableArray arrayWithCapacity:1];
     
     NSString *playlistName;
-        if (playlists.count > 0)
-        {
-            //song exists
-            MPMediaPlaylist *playlist = [playlists objectAtIndex:0];
-            NSArray *songs = [playlist items];
+    if (playlists.count > 0)
+    {
+        //song exists
+        MPMediaPlaylist *playlist = [playlists objectAtIndex:0];
+        NSArray *songs = [playlist items];
+        
+        if (songs.count > 0) {
             
-            if (songs.count > 0) {
+            NSLog(@"Songs Availabe");
+            
+            for (MPMediaItem *song in songs) {
+                NSString *songtId = [song valueForProperty: MPMediaItemPropertyPersistentID];
+                NSString *songName = [song valueForProperty: MPMediaItemPropertyTitle];
                 
-                NSLog(@"Songs Availabe");
+                NSMutableDictionary* so = [NSMutableDictionary dictionaryWithCapacity:1];
                 
-                for (MPMediaItem *song in songs) {
-                    NSString *songtId = [song valueForProperty: MPMediaItemPropertyPersistentID];
-                    NSString *songName = [song valueForProperty: MPMediaItemPropertyTitle];
-                    
-                    NSMutableDictionary* so = [NSMutableDictionary dictionaryWithCapacity:1];
-                    
-                    [so setObject: songName forKey:@"name"];
-                    [so setObject: songtId forKey:@"id"];
-                    
-                    [songsArray addObject:so];
-                }
+                [so setObject: songName forKey:@"name"];
+                [so setObject: songtId forKey:@"id"];
+                
+                [songsArray addObject:so];
             }
-            playlistName= [playlist valueForProperty: MPMediaPlaylistPropertyName];
-            NSLog(@"%@", playlistName);
         }
+        playlistName= [playlist valueForProperty: MPMediaPlaylistPropertyName];
+        NSLog(@"%@", playlistName);
+    }
     
     CDVPluginResult* pluginResult = [CDVPluginResult
                                      resultWithStatus:CDVCommandStatus_OK
                                      messageAsArray:songsArray];
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+- (void)playSong:(CDVInvokedUrlCommand*)command
+{
+    
+    //getPlaylistById
+    NSString *songId = [command.arguments objectAtIndex:0];
+    NSLog(@"songId: %@", songId);
+    
+    MPMediaPropertyPredicate *songIdPredicate =
+    [MPMediaPropertyPredicate predicateWithValue:songId
+                                     forProperty:MPMediaItemPropertyPersistentID];
+    
+    MPMediaQuery *mySongQuery = [MPMediaQuery songsQuery];
+    [mySongQuery addFilterPredicate:songIdPredicate];
+    
+    NSArray *songs = [mySongQuery items];
+    
+    NSString *songName;
+    NSString *songid;
+    if (songs.count > 0)
+    {
+        //song exists
+        MPMediaItem *song = [songs objectAtIndex:0];
+        songName= [song valueForProperty: MPMediaItemPropertyTitle];
+        songid= [song valueForProperty: MPMediaItemPropertyPersistentID];
+        NSURL *url = [song valueForProperty:MPMediaItemPropertyAssetURL];
+        NSLog(@"songName: %@", songName);
+        
+        // Play the item using AVPlayer
+        AVPlayerItem *playerItem = [[AVPlayerItem alloc] initWithURL:url];
+        AVPlayer *player = [[AVPlayer alloc] initWithPlayerItem:playerItem];
+        player.delegate = self;
+        [player play];
+    }
+    
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVPlayer*)player successfully:(BOOL)flag
+{
+    
+    NSString* jsString = nil;
+    if (flag) {
+        jsString = [NSString stringWithFormat:@"%@(\"%s\");", @"cordova.require('cordova-plugin-media.Media').currentCallback", "Audio Completed"];
+    } else {
+        jsString = [NSString stringWithFormat:@"%@(\"%s\");", @"cordova.require('cordova-plugin-media.Media').currentCallback", "Error"];
+    }
+    [self.commandDelegate evalJs:jsString];
 }
 
 
